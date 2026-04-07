f0676efe78f67f574ef7e0ad04545de854e644e1
Abhijith Nuchin <abhijith.nuchin@gmail.com>
2026-04-07 03:53:58 +0000

    Update DataStore to 1.1.1 for preferencesDataStoreDelegate support

--- VoiceIME/app/build.gradle.kts
+++ VoiceIME/app/build.gradle.kts
@@ -56,7 +56,7 @@ dependencies {
     ksp("com.squareup.moshi:moshi-kotlin-codegen:1.15.1")
 
     // DataStore (encrypted prefs)
-    implementation("androidx.datastore:datastore-preferences:1.0.0")
+    implementation("androidx.datastore:datastore-preferences:1.1.1")
     implementation("androidx.security:security-crypto:1.1.0-alpha06")
 
     // Coroutines
